74bc319ecd3b9f2fbc3caa75237e7874f24de6d2
Vlad Nielsen <vlad.nielsen@xoomcloud.com.au>
2026-09-24 15:46:05 +1000

    Change public contact email to support@xoomai.com.au

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -16,8 +16,8 @@ export const SITE = {
   // NAP
   phone: "1300 040 225",
   phoneHref: "tel:1300040225",
-  email: "xoomagent@xoomai.com.au",
-  emailHref: "mailto:xoomagent@xoomai.com.au",
+  email: "support@xoomai.com.au",
+  emailHref: "mailto:support@xoomai.com.au",
   abn: "37 680 921 162",
   parent: "XoomCloud Pty Ltd",
   areaServed: ["Brisbane", "Gold Coast", "Queensland", "Australia"],
